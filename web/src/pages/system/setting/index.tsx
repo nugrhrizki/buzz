00cefdc2fa1eae2646f3fd2dccc6005e6dd8a1b8
nugrhrizki <nugraha07rizki@gmail.com>
2023-12-17 23:42:54 +0700

    update: don't have log yet

--- web/src/pages/system/setting/index.tsx
+++ web/src/pages/system/setting/index.tsx
@@ -1,14 +1,9 @@
-import Datatable from "@/components/ui/datatable";
-
 function SettingPage() {
   return (
     <div class="space-y-4 p-8 pt-6">
       <div class="flex items-center justify-between space-y-2">
         <h2 class="text-3xl font-bold tracking-tight">Setting</h2>
       </div>
-      <Datatable.Root>
-        <Datatable.Table />
-      </Datatable.Root>
     </div>
   );
 }
